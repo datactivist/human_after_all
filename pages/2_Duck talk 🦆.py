7ac49d916364fe772a7fb6245f3d8d594eba8719
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-06-29 16:51:24 +0200

    Update 2_Duck talk 🦆.py

--- pages/2_Duck talk 🦆.py
+++ pages/2_Duck talk 🦆.py
@@ -27,7 +27,7 @@ def app():
     st.title("Duck talk")
     st.subheader('Tell yourself about what you want to do with what yo do')
 
-    st.markdown('Corneille wrote in Polyeucte : "A raconter ses maux, souvent on les soigne", which means that talking to ourselves very often enlightens our problems and gives us key to solve them')
+    st.markdown('Corneille wrote in Polyeucte : "A raconter ses maux, souvent on les soigne", which means that talking to ourselves very often enlightens our problems and gives us key to solve them. Developers talk about the Rubber duck debugging technique 🦆')
 
     
     if "prefix" not in st.session_state:
