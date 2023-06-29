aa2501d1f17df44049c8cf9b8ffb2e24ae4606b2
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-06-29 16:18:03 +0200

    Update 1_Let's go ⏯️ !.py

--- pages/1_Let's go ⏯️ !.py
+++ pages/1_Let's go ⏯️ !.py
@@ -100,7 +100,7 @@ def main():
     
     next_page = st.button("Next step ⏭️ !")
     if next_page:
-        switch_page("Duck_talk 🦆")
+        switch_page("Duck talk 🦆")
 
 if __name__ == "__main__":
     main()
