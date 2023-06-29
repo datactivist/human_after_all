2bc38775a810a9561c5f4af720c8885f283c3ecc
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-06-29 14:43:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st 
 from streamlit_player import st_player
+from streamlit_extras.let_it_rain import rain
+
 
 
 # Streamlit app
@@ -9,6 +11,14 @@ def main():
 
     # Embed a music from SoundCloud
     st_player("https://soundcloud.com/ristanuizuksh/sets/disclosure-you-and-me-flume")
+
+    rain(
+    emoji="❤️",
+    font_size=54,
+    falling_speed=4,
+    animation_length="infinite",
+)
+
 
 if __name__ == "__main__":
     main()
