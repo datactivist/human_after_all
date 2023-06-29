d3eea17e6290594612e20954554294ff8fa92e4c
ArthurSrZ <arthur@datactivi.st>
2023-06-29 14:23:45 +0200

    add_vidéo_player

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import streamlit as st 
+from streamlit_player import st_player
+
 
 # Streamlit app
 def main():
     st.title("Welcome to the Human-Data Interaction simulator")
     st.subheader('Ready to dance with your data ?')
+    
+    # Embed a youtube video
+    st_player("https://youtu.be/CmSKVW1v0xM")
+
+    # Embed a music from SoundCloud
+    st_player("https://soundcloud.com/imaginedragons/demons")
 
 if __name__ == "__main__":
     main()
