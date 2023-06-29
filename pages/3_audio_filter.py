6a157e8f05a282d607f811ab149522b53ce7ba86
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-06-29 16:55:02 +0200

    try lottie

--- pages/3_audio_filter.py
+++ pages/3_audio_filter.py
@@ -1,8 +1,14 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+
 def app():
     st.title("Duck talk")
     st.subheader('Tell yourself about what you want to do with what yo do')
 
     st.markdown('Corneille wrote in Polyeucte : "A raconter ses maux, souvent on les soigne", which means that talking to ourselves very often enlightens our problems and gives us key to solve them. Developers talk about the Rubber duck debugging technique 🦆')
+    
+    with st.echo():
+        st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
 
 
 if __name__ == "__main__":
