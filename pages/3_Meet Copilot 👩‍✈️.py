1277f2ffce6b0c68236ad1925fddd5c76158a2a9
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-06-29 17:37:43 +0200

    Update 3_Meet Copilot 👩‍✈️.py

--- pages/3_Meet Copilot 👩‍✈️.py
+++ pages/3_Meet Copilot 👩‍✈️.py
@@ -2,13 +2,14 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 
 def app():
-    st.title("Duck talk")
-    st.subheader('Tell yourself about what you want to do with what yo do')
+    st.title("Meet your copilot")
+    st.subheader('Enough of talking to data, meet your copilot and collaborate with some human')
+    st_lottie("https://assets10.lottiefiles.com/packages/lf20_Wb5prVKFMW.json", height=300)
 
     st.markdown('Corneille wrote in Polyeucte : "A raconter ses maux, souvent on les soigne", which means that talking to ourselves very often enlightens our problems and gives us key to solve them. Developers talk about the Rubber duck debugging technique 🦆')
     
 
-    st_lottie("https://assets10.lottiefiles.com/packages/lf20_Wb5prVKFMW.json", height=300)
+    
 
 
 if __name__ == "__main__":
