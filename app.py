c1a94a1bcaa4c5db9290848d15ad10d41290b8a7
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-06-29 14:01:53 +0200

    Add Complexity Extractor

--- app.py
+++ app.py
@@ -1,166 +1,87 @@
-"""Object detection demo with MobileNet SSD.
-This model and code are based on
-https://github.com/robmarkcole/object-detection-app
-"""
-
-import logging
-import queue
-from pathlib import Path
-from typing import List, NamedTuple
-
-import av
-import cv2
-import numpy as np
+# Import libraries
 import streamlit as st
-from streamlit_webrtc import WebRtcMode, webrtc_streamer
+import pandas as pd
+from io import StringIO
 
-from sample_utils.download import download_file
-from sample_utils.turn import get_ice_servers
+# Function to calculate the CSV size
+def calculate_csv_size(file):
+    try:
+        # Convert bytes data to a string-based file-like object
+        string_data = file.decode("utf-8")
+        stringio = StringIO(string_data)
 
-HERE = Path(__file__).parent
-ROOT = HERE
+        # Read the CSV file into a DataFrame
+        dataframe = pd.read_csv(stringio)
 
-logger = logging.getLogger(__name__)
+        num_attributes = len(dataframe.columns)
+        num_entities = len(dataframe)
 
+        csv_size = num_attributes * num_entities
+        return csv_size
+    except Exception as e:
+        st.error(f"Error: {e}")
+        return 0
 
-MODEL_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.caffemodel"  # noqa: E501
-MODEL_LOCAL_PATH = ROOT / "./models/MobileNetSSD_deploy.caffemodel"
-PROTOTXT_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.prototxt.txt"  # noqa: E501
-PROTOTXT_LOCAL_PATH = ROOT / "./models/MobileNetSSD_deploy.prototxt.txt"
+# Function to extract the number of relationships
+def extract_relationships(file):
+    try:
+        # Convert bytes data to a string-based file-like object
+        string_data = file.decode("utf-8")
+        stringio = StringIO(string_data)
 
-CLASSES = [
-    "background",
-    "aeroplane",
-    "bicycle",
-    "bird",
-    "boat",
-    "bottle",
-    "bus",
-    "car",
-    "cat",
-    "chair",
-    "cow",
-    "diningtable",
-    "dog",
-    "horse",
-    "motorbike",
-    "person",
-    "pottedplant",
-    "sheep",
-    "sofa",
-    "train",
-    "tvmonitor",
-]
+        # Read the CSV file into a DataFrame
+        dataframe = pd.read_csv(stringio)
 
+        # Calculate the number of relationships
+        num_rows = len(dataframe)
+        num_columns = len(dataframe.columns)
 
-class Detection(NamedTuple):
-    class_id: int
-    label: str
-    score: float
-    box: np.ndarray
+        # Check if the dataset has multi-index
+        if isinstance(dataframe.index, pd.MultiIndex):
+            # Calculate the number of index combinations
+            num_index_combinations = 1
 
+            # Iterate over the index levels to extract unique values
+            for i in range(dataframe.index.nlevels):
+                index_values = dataframe.index.get_level_values(i)
+                num_index_combinations *= len(index_values.unique())
 
-@st.cache_resource  # type: ignore
-def generate_label_colors():
-    return np.random.uniform(0, 255, size=(len(CLASSES), 3))
+            # Calculate the total number of relationships
+            num_relationships = num_rows * num_columns * num_columns * num_index_combinations
+        else:
+            # Calculate the total number of relationships
+            num_relationships = num_rows * num_columns * num_columns
 
-
-COLORS = generate_label_colors()
-
-download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=23147564)
-download_file(PROTOTXT_URL, PROTOTXT_LOCAL_PATH, expected_size=29353)
-
-
-# Session-specific caching
-cache_key = "object_detection_dnn"
-if cache_key in st.session_state:
-    net = st.session_state[cache_key]
-else:
-    net = cv2.dnn.readNetFromCaffe(str(PROTOTXT_LOCAL_PATH), str(MODEL_LOCAL_PATH))
-    st.session_state[cache_key] = net
-
-score_threshold = st.slider("Score threshold", 0.0, 1.0, 0.5, 0.05)
-
-# NOTE: The callback will be called in another thread,
-#       so use a queue here for thread-safety to pass the data
-#       from inside to outside the callback.
-# TODO: A general-purpose shared state object may be more useful.
-result_queue: "queue.Queue[List[Detection]]" = queue.Queue()
+        return num_relationships
+    except Exception as e:
+        st.error(f"Error: {e}")
+        return 0
 
 
-def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-    image = frame.to_ndarray(format="bgr24")
+# Streamlit app
+def main():
+    st.title("Complexity Detector")
+    st.subheader('Find out how complex your dataset is !')
 
-    # Run inference
-    blob = cv2.dnn.blobFromImage(
-        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
-    )
-    net.setInput(blob)
-    output = net.forward()
+    # File selection
+    uploaded_file = st.file_uploader("Upload CSV File", type=["csv"])
+    
+    if uploaded_file is not None:
+        try:
+            # Read file as bytes
+            file = uploaded_file.getvalue()
 
-    h, w = image.shape[:2]
+            # Calculate CSV size
+            csv_size = calculate_csv_size(file)
+            st.subheader("CSV Size")
+            st.write(f"The size of the dataset is: {csv_size}")
 
-    # Convert the output array into a structured form.
-    output = output.squeeze()  # (1, 1, N, 7) -> (N, 7)
-    output = output[output[:, 2] >= score_threshold]
-    detections = [
-        Detection(
-            class_id=int(detection[1]),
-            label=CLASSES[int(detection[1])],
-            score=float(detection[2]),
-            box=(detection[3:7] * np.array([w, h, w, h])),
-        )
-        for detection in output
-    ]
+            # Extract number of relationships
+            num_relationships = extract_relationships(file)
+            st.subheader("Number of Relationships")
+            st.write(f"The number of relationships in the dataset is: {num_relationships}")
+        except Exception as e:
+            st.error(f"Error: {e}")
 
-    # Render bounding boxes and captions
-    for detection in detections:
-        caption = f"{detection.label}: {round(detection.score * 100, 2)}%"
-        color = COLORS[detection.class_id]
-        xmin, ymin, xmax, ymax = detection.box.astype("int")
-
-        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
-        cv2.putText(
-            image,
-            caption,
-            (xmin, ymin - 15 if ymin - 15 > 15 else ymin + 15),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            color,
-            2,
-        )
-
-    result_queue.put(detections)
-
-    return av.VideoFrame.from_ndarray(image, format="bgr24")
-
-
-webrtc_ctx = webrtc_streamer(
-    key="object-detection",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration={
-        "iceServers": get_ice_servers(),
-        "iceTransportPolicy": "relay",
-    },
-    video_frame_callback=video_frame_callback,
-    media_stream_constraints={"video": True, "audio": False},
-    async_processing=True,
-)
-
-if st.checkbox("Show the detected labels", value=True):
-    if webrtc_ctx.state.playing:
-        labels_placeholder = st.empty()
-        # NOTE: The video transformation with object detection and
-        # this loop displaying the result labels are running
-        # in different threads asynchronously.
-        # Then the rendered video frames and the labels displayed here
-        # are not strictly synchronized.
-        while True:
-            result = result_queue.get()
-            labels_placeholder.table(result)
-
-st.markdown(
-    "This demo uses a model and code from "
-    "https://github.com/robmarkcole/object-detection-app. "
-    "Many thanks to the project."
-)
+if __name__ == "__main__":
+    main()
